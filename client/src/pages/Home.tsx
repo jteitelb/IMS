0b8903a12110ce827f877a51eb894e2a551a9eb1
jteitelb <7592143+jteitelb@users.noreply.github.com>
2023-09-08 13:47:44 -0400

    add quick link to dashboard for testing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      Home
+    <div>
+      <Link to="dashboard">
+        <div className="w-full h-64 bg-slate-900 flex justify-center items-center text-slate-300 text-xl">
+          <div>Dashboard</div>
+        </div>
+      </Link>
+      <div className="flex items-center justify-center w-screen h-screen">
+        Home
+      </div>
     </div>
   );
 };
