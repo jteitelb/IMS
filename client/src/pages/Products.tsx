3df9cb022bb79d16e5ef1fe2a1f87a1056e2de41
jteitelb <7592143+jteitelb@users.noreply.github.com>
2023-09-10 01:05:43 -0400

    fetch existing products and display in table

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -2,8 +2,22 @@ import * as Yup from "yup";
 import Form from "../components/Form/Form";
 import FormField from "../components/Form/FormField";
 import SubmitButton from "../components/Form/SubmitButton";
+import { useState, useEffect } from "react";
 
 const Products = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/products")
+      .then((res) => res.json().then((json) => setProductList(json)))
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }, []);
+
+  console.log(productList);
+
   const initialValues = {
     partno: "",
     item: "",
@@ -36,6 +50,30 @@ const Products = () => {
         <FormField label="Amount" name="amount" type="number" />
         <SubmitButton title="Add" />
       </Form>
+
+      <h2 className="mt-5 text-lg font-bold">Current Items</h2>
+      <table className="w-3/4 border-2 border-slate-400 text-left">
+        <thead>
+          <tr>
+            <th>Part No</th>
+            <th>Item</th>
+            <th>UOM</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productList.map(({ partno, item, uom, amount }) => {
+            return (
+              <tr className="even:bg-slate-200 odd:bg-white">
+                <td>{partno}</td>
+                <td>{item}</td>
+                <td>{uom}</td>
+                <td>{amount}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
